08e640c00d2abde6b4ee3ae18dea4aef53630a8d
Carlos Aponte <cdaponte95@gmail.com>
2021-03-23 01:51:23 -0400

    banner title, description, and truncate function for the description

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios';
 import requests from './requests';
+import './Banner.css';
 
 function Banner(){
     const [movie, setMovie] = useState([]);
@@ -17,6 +18,10 @@ function Banner(){
 
     console.log(movie);
 
+    function truncate(str, n){
+        return str?.length > n ? str.substr(0, n -1 ) + "..." : str;
+    }
+
     return(
         <header className="banner" style={{
             backgroundSize:'cover',
@@ -24,12 +29,12 @@ function Banner(){
             backgroundPosition:"center center",
         }}>
             <div className="banner__contents">
-                <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+                <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
                 <div className="banner__buttons">
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
-                {/* {Description} */}
+                <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
             </div>
         </header>
     );
